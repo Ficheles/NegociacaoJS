e5599edffb7fa274bdf4804a7dd3b0e4ec838239
Rafael Fideles <rafaelfideles@live.com>
2018-11-07 00:35:39 -0200

    Criado arquivo Negociacao e realizado o teste da versão 0

---
+++ client/app/domain/negociacao/Negociacao.js
@@ -0,0 +1,10 @@
+// client/app/domain/negociacao/Negociacao.js
+
+class Negociacao {
+
+	constructor(data, quantidade, valor) {
+		this.data = new Date(); // data atual
+		this.quantidade = 1;
+		this.valor = 0.0;
+	}
+}
