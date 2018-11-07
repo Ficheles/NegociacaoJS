621c0b9e6ef4cdf131981d9ea1770fe517bd10a4
Rafael Fideles <rafaelfideles@live.com>
2018-11-07 01:55:11 -0200

    Alterando o construtor para reduzir a verbosidade atraves do Object.assign

--- client/app/domain/negociacao/Negociacao.js
+++ client/app/domain/negociacao/Negociacao.js
@@ -3,8 +3,29 @@
 class Negociacao {
 
 	constructor(data, quantidade, valor) {
-		this.data = new Date(); // data atual
-		this.quantidade = 1;
-		this.valor = 0.0;
+		
+		// cada parametro recebido será atribuido as propriedades da classe
+		// 
+		this._data = new Date(data.getTime()); // data atual
+		this._quantidade = quantidade;
+		this._valor = valor;
+
+		Object.freeze(this);
+	}
+
+	get volume() {
+		return this._quantidade * this._valor;
+	}
+
+	get data() {
+		return new Date(this._data.getTime());
+	}
+
+	get quantidade() {
+		return this._quantidade;
+	}
+
+	get valor() {
+		return this._valor;
 	}
 }
